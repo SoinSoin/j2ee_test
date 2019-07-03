aa131c76ff2bb1497b0bda320e1ee65d8fe9d6b2
soinsoin <vincentretore@mbp-vincent-r.home>
2019-07-03 14:55:39 +0200

    pass to gradle

---
+++ Docker/jdk/docker-entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Build backend"
+gradle -t build ;
---
+++ app/backend/src/main/java/api/hello/HelloController.java
@@ -0,0 +1,17 @@
+package api.hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/hello")
+public class HelloController {
+    private static final String template = "change, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping
+    public Hello hello(@RequestParam(value="name", defaultValue="new") String name) {
+        return new Hello(counter.incrementAndGet(), String.format(template, name));
+    }
+}
